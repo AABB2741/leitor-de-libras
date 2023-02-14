4e7140c71785c9ced4395b3181147905ed7da601
AABB2741 <mariosantos88477364484@gmail.com>
2023-02-14 15:25:45 -0300

    Remover animações no popup tbm

--- leitor-de-libras/src/components/Popup/index.tsx
+++ leitor-de-libras/src/components/Popup/index.tsx
@@ -9,6 +9,8 @@ import * as Animatable from "react-native-animatable";
 
 import { useColors } from "../../contexts/colors";
 import { useLang } from "../../contexts/lang";
+import { useSettings } from "../../contexts/settings";
+
 import Button from "../Button";
 import Font from "../Font";
 
@@ -30,6 +32,7 @@ interface PopupProps extends ModalProps {
 
 export default function Popup({ type = "ok", title, text, caution, children, loading, onRespondOk, onRespondConfirm, onRespondBoolean, ...rest }: PopupProps) {
     const lang = useLang();
+    const { settings } = useSettings();
     const colors = useColors();
     const styles = createStyles({ colors });
     
@@ -44,7 +47,7 @@ export default function Popup({ type = "ok", title, text, caution, children, loa
             >
                 <Pressable style={styles.background} onPress={rest.onRequestClose} />
                 <View style={styles.container}>
-                    <Animatable.View style={styles.content} animation="zoomIn" duration={300}>
+                    <Animatable.View style={styles.content} animation={settings.display.performance.reduce_animations ? undefined : "zoomIn"} duration={300}>
                         <View>
                             {title && <Font preset="subtitle" style={styles.title}>{title}</Font>}
                             {text && <Font preset="text" style={styles.text}>{text}</Font>}
